65fd5cd83be657d7018a8be1c3d3effccc3c94aa
Jack Bracken <contact@jacktrick.com>
2013-11-18 10:35:46 +0000

    Test ParseVoteFile

--- src/main/java/me/jackbracken/FYP/FYP.java
+++ src/main/java/me/jackbracken/FYP/FYP.java
@@ -3,10 +3,12 @@ package main.java.me.jackbracken.FYP;
 import java.io.File;
 import java.util.Vector;
 
-import main.java.me.jackbracken.FYP.FileUtilities.ParsePostsFile;
+import main.java.me.jackbracken.FYP.FileUtilities.ParsePostFile;
 import main.java.me.jackbracken.FYP.FileUtilities.ParseUserFile;
+import main.java.me.jackbracken.FYP.FileUtilities.ParseVoteFile;
 import main.java.me.jackbracken.FYP.Models.Post;
 import main.java.me.jackbracken.FYP.Models.User;
+import main.java.me.jackbracken.FYP.Models.Vote;
 
 import com.googlecode.flyway.core.Flyway;
 
@@ -18,7 +20,9 @@ public class FYP {
 		flyway.setDataSource("jdbc:postgresql:fyp", "karma", "karma");
 		flyway.migrate();
 
-		File userFile = new File("/home/jack/stack/photo/Users.xml");
+		final String HOME = System.getenv("HOME");
+		
+		File userFile = new File(HOME + "/stack/photo/Users.xml");
 
 		ParseUserFile puf = new ParseUserFile(userFile);
 		Vector<User> users = new Vector<User>(puf.getUsers());
@@ -26,9 +30,9 @@ public class FYP {
 			System.out.println(u.toString());
 		}
 		
-		File postFile = new File("/home/jack/stack/photo/Posts.xml");
+		File postFile = new File(HOME + "/stack/photo/Posts.xml");
 		
-		ParsePostsFile ppf = new ParsePostsFile(postFile);
+		ParsePostFile ppf = new ParsePostFile(postFile);
 		Vector<Post> posts = new Vector<Post>(ppf.getPosts());
 		for(Post p: posts) {
 			if(p.getAnswerCount() == 0){
@@ -36,6 +40,13 @@ public class FYP {
 			}
 		}
 		
+		File voteFile = new File(HOME + "/stack/photo/Votes.xml");
+		
+		ParseVoteFile pvf = new ParseVoteFile(voteFile);
+		Vector<Vote> votes = new Vector<Vote>(pvf.getVotes());
+		for(Vote v: votes) {
+			System.out.println(v.toString());
+		}
 		
 		System.exit(0);
 	}
