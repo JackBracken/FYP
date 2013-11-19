7238d344740e0d6a19a71c1a79ce9131e012358e
Jack Bracken <contact@jacktrick.com>
2013-11-19 04:25:51 +0000

    Move file parsing logic to launcher class

--- src/main/java/me/jackbracken/FYP/FYP.java
+++ src/main/java/me/jackbracken/FYP/FYP.java
@@ -1,14 +1,8 @@
 package main.java.me.jackbracken.FYP;
 
 import java.io.File;
-import java.util.Vector;
 
-import main.java.me.jackbracken.FYP.FileUtilities.ParsePostFile;
-import main.java.me.jackbracken.FYP.FileUtilities.ParseUserFile;
-import main.java.me.jackbracken.FYP.FileUtilities.ParseVoteFile;
-import main.java.me.jackbracken.FYP.Models.Post;
-import main.java.me.jackbracken.FYP.Models.User;
-import main.java.me.jackbracken.FYP.Models.Vote;
+import main.java.me.jackbracken.FYP.FileUtilities.ParserLauncher;
 
 import com.googlecode.flyway.core.Flyway;
 
@@ -22,31 +16,8 @@ public class FYP {
 
 		final String HOME = System.getenv("HOME");
 		
-		File userFile = new File(HOME + "/stack/photo/Users.xml");
-
-		ParseUserFile puf = new ParseUserFile(userFile);
-		Vector<User> users = new Vector<User>(puf.getUsers());
-		for(User u: users) {
-			System.out.println(u.toString());
-		}
-		
-		File postFile = new File(HOME + "/stack/photo/Posts.xml");
-		
-		ParsePostFile ppf = new ParsePostFile(postFile);
-		Vector<Post> posts = new Vector<Post>(ppf.getPosts());
-		for(Post p: posts) {
-			if(p.getAnswerCount() == 0){
-				System.out.println(p.toString());
-			}
-		}
-		
-		File voteFile = new File(HOME + "/stack/photo/Votes.xml");
-		
-		ParseVoteFile pvf = new ParseVoteFile(voteFile);
-		Vector<Vote> votes = new Vector<Vote>(pvf.getVotes());
-		for(Vote v: votes) {
-			System.out.println(v.toString());
-		}
+		File dataRoot = new File(HOME + "/stack/");
+		new ParserLauncher(dataRoot);
 		
 		System.exit(0);
 	}
---
+++ src/main/java/me/jackbracken/FYP/FileUtilities/ParserLauncher.java
@@ -0,0 +1,41 @@
+package main.java.me.jackbracken.FYP.FileUtilities;
+
+import java.io.File;
+
+public class ParserLauncher {
+	// dataRoot should be the directory with the unzipped stack 
+	// exchange files, each site in its own sub-directory
+	
+	public ParserLauncher(File dataRoot) {
+		String fileName;
+		File[] listOfSites = dataRoot.listFiles();
+		
+		for(File siteDirectory : listOfSites) {
+			if(!siteDirectory.isFile()) {
+				File[] siteDataFiles = siteDirectory.listFiles();
+				
+				System.out.println("Directory:\t" + siteDirectory.getName());
+				for (File dataFile: siteDataFiles) {
+					fileName = dataFile.getName();
+					
+					if (fileName.contains("Users")) {
+						System.out.println("Parsing file:\t" + fileName);
+						new ParseUserFile(dataFile);
+					} else if (fileName.contains("Posts")) {
+						System.out.println("Parsing file:\t" + fileName);
+						new ParsePostFile(dataFile);
+					} else if (fileName.contains("Votes")) {
+						System.out.println("Parsing file:\t" + fileName);
+						new ParseVoteFile(dataFile);
+					} else {
+						System.out.println("Discarding file:\t" + fileName);
+					}
+				}
+				
+				
+			} else {
+				System.out.println("Erroneous file " + siteDirectory.getName() + "found.");
+			}
+		}
+	}
+}
